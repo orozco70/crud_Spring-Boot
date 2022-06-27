8e7e2631e5f2e0b678418ae79c75f024f42ea743
Adriel Pérez Orozco <adriel.perez@oc.emcomed.cu>
2022-06-27 11:46:39 -0400

    Crud producto

--- src/main/java/directorio/demo/Controller/ProdControl.java
+++ src/main/java/directorio/demo/Controller/ProdControl.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ch.qos.logback.core.status.Status;
 import directorio.demo.Services.ProducImpl;
@@ -33,40 +36,60 @@ public class ProdControl {
 	}
 
 	/*
-	 * @RequestMapping(value ="/test{nombre}", method = RequestMethod.GET) public
-	 * ResponseEntity<List<Producto>> test(@PathVariable("nombre") String nombre) {
-	 * List<Producto> cli =prodrepo.findByNombre(nombre); return
-	 * ResponseEntity.ok(cli); }
-	 */
-	/*
-	 * @RequestMapping(value ="/test{nombre}", method = RequestMethod.GET) public
-	 * ResponseEntity<Iterable<Producto>> test(String nombre) {
+	 * @RequestMapping(value = "/test{nombre}", method = RequestMethod.GET) public
+	 * ResponseEntity<?> test(String nombre) {
 	 * 
-	 * Iterable<Producto> cli = null;
+	 * try { return
+	 * ResponseEntity.status(HttpStatus.OK).body(prod_impl.listapornombre(nombre));
 	 * 
-	 * try { cli =prod_impl.listapornombre(nombre);
+	 * } catch (Exception e) { return
+	 * ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"Error\": \"" +
+	 * e.getMessage() + "\" }")); }
 	 * 
-	 * } catch (Exception e) { // TODO: handle exception }
-	 * 
-	 * 
-	 * return ResponseEntity.ok(cli); }
+	 * }
 	 */
 
-	@RequestMapping(value = "/test{nombre}", method = RequestMethod.GET)
-	public ResponseEntity<?> test(String nombre) {
+	@RequestMapping(value = { "/", "", "show" }, method = RequestMethod.GET)
+	public String mostrar(Model model) {
+		List<Producto> prod = (List<Producto>) prod_impl.findall();
+		model.addAttribute("prodlist", prod);
+		return "producto/lista";
+	}
 
-		try {
-			return ResponseEntity.status(HttpStatus.OK).body(prod_impl.listapornombre(nombre));
+	@RequestMapping(value = { "/add/{id}" }, method = RequestMethod.GET)
+	public String add(Model model, @PathVariable("id") Long id) {
 
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"Error\": \"" + e.getMessage() + "\" }"));
+		if (id == 0) {
+			model.addAttribute("titulo", "Agregar Producto");
+			model.addAttribute("producto", new Producto());
+
+		} else {
+
+			model.addAttribute("titulo", "Modificar Producto");
+			model.addAttribute("producto", prod_impl.findbyid(id));
+
 		}
 
+		return "producto/add";
 	}
 
 	@PostMapping("/save")
-	public void save(Producto pr) {
+	public String save(Producto pr, BindingResult result, RedirectAttributes ra) {
+		if (result.hasErrors()) {
+
+			return "prod/add";
+		}
 		prod_impl.agregar(pr);
+		ra.addFlashAttribute("MsgExito", "Guardado con Exito");
+		return "redirect:/prod/show";
 
 	}
+
+	@GetMapping("/del/{id}")
+	public String eliminar(@PathVariable Long id, RedirectAttributes ra) {
+		prod_impl.eliminar(id);
+		ra.addFlashAttribute("msgExito", "Eliminado Correcto");
+		return "redirect:/prod/show";
+	}
+
 }
